2bc4791e5967623652cafc1fae9997118dca9045
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-06-09 11:06:12 +0300

    resetup

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header
+      style={{
+        padding: "1rem",
+        backgroundColor: "#f5f5f5",
+        textAlign: "center",
+      }}
+    >
+      <h1>My Next.js Project Header</h1>
+    </header>
+  );
+};
+
+export default Header;
