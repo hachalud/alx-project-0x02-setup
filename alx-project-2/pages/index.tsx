19ca5560cbb2d4f11b2a329b6ebf50352d90be03
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-06-09 10:56:34 +0300

    setup

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Welcome to My Next.js Project!</h1>
+      <p>Get started by exploring the basics of Next.js.</p>
+    </main>
+  );
+};
+
+export default Home;
